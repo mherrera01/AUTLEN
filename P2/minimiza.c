460b70c94db790ac00ab96039ed85cedb0b65427
mherrera01 <48330260+mherrera01@users.noreply.github.com>
2020-12-05 22:01:22 +0100

    Transitions matrix implemented

--- P2/minimiza.c
+++ P2/minimiza.c
@@ -4,6 +4,11 @@
 descartando así los estados inaccesibles del afd */
 List* obtenerEstadosAccesibles(AFND* afd);
 
+/* Función que crea una matriz con todas las transiciones entre estados del afd */
+int** matrizTransiciones(AFND* afd);
+/* Función que libera la memoria de la matriz de transiciones */
+void freeMatriz(AFND* afd, int** matriz);
+
 AFND* AFNDMinimiza(AFND* afd){
     /* Afd minimizado */
     AFND* afd_min = NULL;
@@ -19,10 +24,12 @@ AFND* AFNDMinimiza(AFND* afd){
 
     int num_estados, num_simbolos, num_estados_accesibles;
     int tipoEstado;
-    int* estado = NULL;
+    int coincidencias;
+    int* estado = NULL, *estado2 = NULL;
+    int** transiciones = NULL;
 
     /* Contadores de loop */
-    int i, j, k;
+    int i, j, k, h;
 
     /* Creamos un archivo txt con mensajes de la ejecución del minimiza */
     dlog_init();
@@ -145,35 +152,138 @@ AFND* AFNDMinimiza(AFND* afd){
     list_destroy(clase_noFinales);
     list_destroy(clase_finales);
 
+    /* Obtenemos la matriz con todas las transiciones entre estados del afd */
+    transiciones = matrizTransiciones(afd);
+    if (transiciones == NULL){
+        dlog("ERROR: ");
+        list_destroy(estados);
+        return NULL;
+    }
+
     /* Recorremos todas las clases de los estados del afd para juntar los estados equivalentes */
     for (i=0; i < list_size(estados); i++){
         clase = list_get(estados, i);
         if (clase == NULL){
             sprintf(Message, "ERROR: La clase en la posición %d a procesar no se consiguió correctamente", i);
             dlog(Message);
+            freeMatriz(afd, transiciones);
             list_destroy(estados);
             return NULL;
         }
 
-        for (j=0; j < num_simbolos; j++){
-            for (k=0; k < num_estados_accesibles; k++){
+        /*
+        A ->
+        B -> A
+        C -> A B
+        D -> A B C
+        */
 
-                /* Vemos si se puede transitar desde el estado en la posición i al k con el símbolo j */
+        /* Comprobamos si los estados son equivalentes, viendo que desde cada clase se puede transitar a elementos de la misma clase */
+        for (k=0; k < list_size(clase); k++){
+            estado = list_get(clase, k);
+            if (estado == NULL){
+                sprintf(Message, "ERROR: El estado de la clase %d en la posición %d a procesar no se consiguió correctamente", i, k);
+                dlog(Message);
+                freeMatriz(afd, transiciones);
+                list_destroy(estados);
+                return NULL;
+            }
+
+            for (h = k-1; h >= 0; h--){
+                estado2 = list_get(clase, h);
+                if (estado2 == NULL){
+                    sprintf(Message, "ERROR: El estado de la clase %d en la posición %d a procesar no se consiguió correctamente", i, h);
+                    dlog(Message);
+                    freeMatriz(afd, transiciones);
+                    list_destroy(estados);
+                    return NULL;
+                }
+
+                coincidencias = 0;
+                for (j=0; j < num_simbolos; j++){
+                    /*
+                    [A B C D] - [E, F] [G, H]
+
+                    A --> 0 B
+                    B --> 0 C
+                    */
+                    if (transiciones[*estado][j] == transiciones[*estado2][j]){
+                        coincidencias++;                        
+                    }
+                }
+
+                /* Son la misma clase */
+                if(num_simbolos == coincidencias);
             }
         }
     }
+
     dlog("\nOK: Afd minimizado con éxito");
     dlog("Convirtiendo el afd minimizado a la estructura de la librería...");
 
     /* Convertimos el afd minimizado de nuestra estructura a la de la librería */
 
     /* Liberamos memoria */
+    freeMatriz(afd, transiciones);
     list_destroy(estados);
 
     dlog("\nOK: Afd convertido con éxito");
     dlog("Termina la ejecución correctamente");
 
     return afd_min;
+}
+
+int** matrizTransiciones(AFND* afd){
+    int **matriz = NULL;
+    int i, j, k;
+
+    /* Creamos una matriz de enteros para las transiciones del afd */
+    matriz = (int**) malloc (AFNDNumEstados(afd) * sizeof(int*));
+    if (matriz == NULL){
+        dlog("ERROR: La matriz de transiciones del afd no se creó correctamente");
+        return NULL;
+    }
+
+    /* Reservamos memoria para cada fila de la matriz */
+    for(i = 0; i < AFNDNumEstados(afd); i++){
+        matriz[i] = (int*) malloc (AFNDNumSimbolos(afd) * sizeof(int));
+        if (matriz[i] == NULL){
+            dlog("ERROR: Una transición del afd no se añadió correctamente a la matriz de transiciones");
+            freeMatriz(afd, matriz);
+            return NULL;
+        }
+    }
+
+    /* Guardamos en cada casilla de la matriz a qué estado transitaría desde el estado i con el símbolo j */
+    for (i = 0; i < AFNDNumEstados(afd); i++){
+        for (j = 0; j < AFNDNumSimbolos(afd); j++){
+            matriz[i][j] = -1;
+            for (k = 0; k < AFNDNumEstados(afd); k++){
+                if (AFNDTransicionIndicesEstadoiSimboloEstadof(afd, i, j, k)){
+                    matriz[i][j] = k;
+                    printf("%d transita a %d con %d. Como lo oyes.\n", i, matriz[i][j], j);
+                }
+            }
+        }
+    }
+
+    return matriz;
+}
+
+void freeMatriz(AFND *afd, int **matriz){
+    int i;
+
+    if (matriz != NULL){
+        for(i = 0; i < AFNDNumEstados(afd); i++){
+            if (matriz[i] != NULL){
+                /* Liberamos la memoria de la fila int* de la matriz */
+                free(matriz[i]);
+            }
+        }
+        
+        /* Liberamos la memoria del int** matriz */
+        free(matriz);
+    }
 }
 
 List* obtenerEstadosAccesibles(AFND* afd){
